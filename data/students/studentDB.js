eca3acea8216f67689f76e2f07d36c75e0f91c48
Leila <nb.leila10@gmail.com>
2019-03-19 19:37:55 -0500

    Implement the GET student API
    Endpoint: /students/:id
    Returns the student with the specified id
    Return the cohort name instead of the cohort id
    Throws error if no student with that id, or is problem with database

--- data/students/studentDB.js
+++ data/students/studentDB.js
@@ -3,9 +3,23 @@ const knexConfig = require("../../knexfile.js")
 const db = knex(knexConfig.development)
 
 module.exports = {
-  find
+  find,
+  findById,
+  insert
 }
 
 function find() {
   return db("students")
+}
+
+function insert(student) {
+  return db("students")
+    .insert(student)
+    .then(ids => ({ id: ids[0] }))
+}
+
+function findById(id) {
+  return db("students")
+    .where({ id: Number(id) })
+    .first()
 }
--- routers/students/students_router.js
+++ routers/students/students_router.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 const studentDB = require("../../data/students/studentDB")
+const cohortDB = require("../../data/cohorts/cohortDB")
 
 router.get("/", (req, res) => {
   studentDB
@@ -14,6 +15,59 @@ router.get("/", (req, res) => {
         .status(500)
         .json({ error: "The students information could not be retrieved." })
     )
+})
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params
+  studentDB
+    .findById(id)
+    .then(student => {
+      if (student) {
+        const cohortId = student.cohort_id
+        let returnedStudent = {
+          id: student.id,
+          name: student.name,
+          cohort: ""
+        }
+        cohortDB
+          .findById(cohortId)
+          .then(cohort => {
+            if (cohort) {
+              returnedStudent.cohort = cohort.name
+            }
+            res.status(200).json(returnedStudent)
+          })
+          .catch(err => console.log(err))
+      } else
+        res
+          .status(400)
+          .json({ errorMessage: "Please provide a valid id for the student." })
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The student information could not be retrieved." })
+    )
+})
+
+router.post("/", (req, res) => {
+  const studentInfo = req.body
+  if (!studentInfo.name || !studentInfo.cohort_id) {
+    res.status(400).json({
+      errorMessage: "Please provide name and cohort_id for the student."
+    })
+  } else {
+    studentDB
+      .insert(studentInfo)
+      .then(studentId => {
+        res.status(201).json(studentId)
+      })
+      .catch(err =>
+        res.status(500).json({
+          error: "There was an error while saving the student to the database"
+        })
+      )
+  }
 })
 
 module.exports = router
